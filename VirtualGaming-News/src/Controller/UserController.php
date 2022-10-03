9f9696f2abb8ddd56b222d919f71c3b6913b4985
gauthier.L <gauthier.largi@gmail.com>
2022-10-03 15:40:05 +0200

    troistest

--- VirtualGaming-News/src/Controller/UserController.php
+++ VirtualGaming-News/src/Controller/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use Agit pp\Form\UserRegisterType;
+use App\Entity\user;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
