662a0f3acd82d05183cd0dc539529e4aadaf1a11
gauthier.L <gauthier.largi@gmail.com>
2022-10-07 19:48:42 +0200

    test

--- VirtualGaming-News/src/Controller/MainController.php
+++ VirtualGaming-News/src/Controller/MainController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\GameRepository;
+use App\Repository\PostRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,21 +12,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository, GameRepository $gameRepository,PostRepository $postRepository): Response
     {
         // recupérer tout mes jeux *array rand tableau
+        $category = $categoryRepository->findAll();
+        $games = $gameRepository->findAll();
+        $post = $postRepository->findAll();
         
-        return $this->render('main/index.html.twig');
+        return $this->render('main/index.html.twig',[
+            
+            'category' => $category, 'game' => $games, 'post' => $post]);
+
     }
+
     #[Route('/cgu', name: 'cgu')]
     public function cgu(): Response
     {
+
         return $this->render('main/cgu.html.twig');
+
     }
+
     #[Route('/about', name: 'about')]
     public function about(): Response
     {
+
         return $this->render('main/about.html.twig');
+
     }
 
 }
