117c0f32e09df4beeda35bbabb4f4c94dcb5ca1a
gauthier.L <gauthier.largi@gmail.com>
2022-10-11 13:52:48 +0200

    savejury

--- VirtualGaming-News/migrations/Version20221011111839.php
+++ VirtualGaming-News/migrations/Version20221011111839.php
@@ -7,7 +7,10 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20221011080609 extends AbstractMigration
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221011111839 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -16,7 +19,7 @@ final class Version20221011080609 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, type VARCHAR(50) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, comments_id INT NOT NULL, comments_posts_id INT NOT NULL, text LONGTEXT NOT NULL, status VARCHAR(50) NOT NULL, INDEX IDX_9474526C63379586 (comments_id), INDEX IDX_9474526C473D1FAD (comments_posts_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE game (id INT AUTO_INCREMENT NOT NULL, users_id INT DEFAULT NULL, name VARCHAR(50) NOT NULL, price NUMERIC(15, 2) NOT NULL, total_weight INT DEFAULT NULL, duration NUMERIC(15, 2) NOT NULL, picture VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, develop VARCHAR(50) NOT NULL, status VARCHAR(50) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', update_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_232B318C67B3B43D (users_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -37,7 +40,7 @@ final class Version20221011080609 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        
+        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C63379586');
         $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C473D1FAD');
         $this->addSql('ALTER TABLE game DROP FOREIGN KEY FK_232B318C67B3B43D');
