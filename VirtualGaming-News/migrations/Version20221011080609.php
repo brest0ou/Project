444bf0ce2a573e00029f83852148dda0319b9db2
unknown <didi.59100@live.fr>
2022-10-11 12:50:44 +0200

    l

--- VirtualGaming-News/migrations/Version20221011080609.php
+++ VirtualGaming-News/migrations/Version20221011080609.php
@@ -7,7 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-
 final class Version20221011080609 extends AbstractMigration
 {
     public function getDescription(): string
